8b6fbf25e2e84308ca8b48b55049f784d6824f4c
Arfelo Reed Torralba <CPAMaster1994@gmail.com>
2024-08-17 12:43:05 +0800

    added script for dowloading yt vids through python

---
+++ yt-download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd ~/Documents/python-projects/ytVid-dl
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Run the Python script
+python3 app.py
+
+# Deactivate the virtual environment after the script execution
+deactivate
+
